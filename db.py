734001bbe14d02318803565262fbdb09fbfad7a3
Oleksandr <merlin.ua.1@gmail.com>
2024-04-29 03:22:35 +0300

    Add service.py, db.py

---
+++ db.py
@@ -0,0 +1,94 @@
+import mysql.connector
+
+
+class DB:
+    def __init__(self, host: str = '127.0.0.1', port: int = 3306, user: str = 'root', password: str = 'qwertyuiop',
+                 database: str = 'project-d-db'):
+        self.cnx = None
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def __del__(self):
+        if self.cnx is not None:
+            self.db_close()
+
+    def db_init(self):
+        print('DB Init...')
+        try:
+            # Connect to server
+            self.cnx = mysql.connector.connect(
+                host=self.host,
+                port=self.port,
+                user=self.user,
+                password=self.password,
+                database=self.database)
+
+            if not self.cnx.is_connected():
+                raise mysql.connector.Error('Failed to connect to MySQL server.')
+
+            print('Connected to MySQL server.')
+
+            # Get a cursor
+            cur = self.cnx.cursor()
+            print('Cursor created')
+
+            # Execute a query
+            cur.execute("SELECT CURDATE()")
+
+            # Fetch one result
+            row = cur.fetchone()
+            print("Current date is: {0}".format(row[0]))
+
+            # Create a REQUESTS table
+            cur.execute('''
+                CREATE TABLE IF NOT EXISTS REQUESTS (
+                    ID INT NOT NULL AUTO_INCREMENT,
+                    NAME VARCHAR(255) NOT NULL,
+                    PRIMARY KEY (ID)
+                );
+            ''')
+            print('REQUESTS table created')
+
+            # Update REQUESTS table
+            cur.execute('''
+                INSERT INTO REQUESTS (ID, NAME) VALUES (1, 'Iphone 12 64GB Blue')
+                ON DUPLICATE KEY UPDATE NAME = 'Iphone 12 64GB Blue';
+            ''', {'raise_on_warnings': True})
+
+            # Make sure data is committed to the database
+            self.cnx.commit()
+            print('REQUESTS table updated')
+
+        except mysql.connector.Error as e:
+            print('DB Init Error:', e)
+            exit(0)
+        else:
+            print('DB Init successful')
+            cur.close()
+
+    def get_requests(self):
+        try:
+            # Get a cursor
+            cur = self.cnx.cursor()
+
+            # Execute a query
+            cur.execute('SELECT * FROM REQUESTS')
+
+            # Fetch all results
+            rows = cur.fetchall()
+            cur.close()
+            return rows
+
+        except mysql.connector.Error as e:
+            print('Get Requests Error:', e)
+            return []
+
+    def db_close(self):
+        try:
+            self.cnx.close()
+            print('DB Connection closed')
+        except mysql.connector.Error as e:
+            print('DB Close Error:', e)
---
+++ service.py
@@ -0,0 +1,84 @@
+import time
+from threading import Thread
+
+import schedule
+import uvicorn
+from fastapi import FastAPI
+
+from db import DB
+
+# DB connection parameters
+db_host: str = '127.0.0.1'
+db_port: int = 3306
+db_user: str = 'root'
+db_password: str = 'qwertyuiop'
+db_database: str = 'project-d-db'
+
+# Initialize DB
+db = DB(db_host, db_port, db_user, db_password, db_database)
+
+# API parameters
+api_host: str = '127.0.0.1'
+api_port: int = 8000
+
+# Initialize FastAPI
+api = FastAPI()
+
+
+@api.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@api.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@api.get("/db/requests")
+async def get_requests():
+    return {"rows": db.get_requests()}
+
+
+def run_api():
+    uvicorn.run(api, host=api_host, port=api_port)
+    print('API is running...')
+
+
+def init():
+    print('project-d service is initializing...')
+    db.db_init()
+    print('project-d service is initialized.')
+
+
+def update_db():
+    print('Updating DB...')
+    print('Current time: ', time.ctime())
+    print('Requests: ', db.get_requests())
+    print('DB updated.')
+
+
+if __name__ == '__main__':
+    init()
+    print('project-d service is running...')
+
+    # Run the API
+    api_thread = Thread(target=run_api, daemon=True)
+    api_thread.start()
+
+    # Schedule the job to run every minute
+    schedule.every().minute.do(update_db)
+    print('DB update scheduled.')
+
+    try:
+        # Run the scheduler
+        while True:
+            schedule.run_pending()
+            time.sleep(1)  # Sleep for 1 second to avoid high CPU usage
+    except KeyboardInterrupt:
+        print('keyboard interrupt')
+    finally:
+        print('project-d service is stopping...')
+        db.db_close()
+        print('project-d service stopped.')
+        exit(0)
